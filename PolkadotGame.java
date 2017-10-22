ebfabba9706c0fccc0be49133517b4ad6dd490c2
Luke Thistlethwaite <lukethistle101@gmail.com>
2017-10-21 20:09:28 -0400

    added classes

--- PolkadotGame.java
+++ PolkadotGame.java
@@ -15,3 +15,58 @@ public class PolkadotGame
       polkadotFrame.setVisible(true);
    }
 }
+
+class PolkadotPanel extends JPanel
+{
+   
+}
+
+class MouseDot extends Circle
+{
+   public MouseDot(int size, int x, int y, Color color)
+   {
+      super(size,x,y,color);
+   }
+}
+
+class Circle
+{
+   private double diameter;
+   private double myX;  
+   private double myY;
+   private Color myColor;
+   
+   public Circle(double size, double x, double y, Color color)
+   {
+      diameter = size;
+      myX = x;
+      myY = y;
+      myColor = color;
+   }
+   
+   public double getX()
+   {
+      return myX;
+   }
+   
+   public double getY()
+   {
+      return myY;
+   }
+   
+   public double getDiameter()
+   {
+      return diameter;
+   }
+   
+   public double getRadius()
+   {
+      return getDiameter()/2;
+   }
+   
+   public void drawme(Graphics myBuffer) 
+   {
+      myBuffer.setColor(this.myColor);
+      myBuffer.fillOval((int)(getX() - getRadius()), (int)(getY()-getRadius()), (int)getDiameter(), (int)getDiameter());
+   }
+}
